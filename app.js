fb46d45f845cb044be172d9b08de9c6fe31d0967
AliHasan06 <alihassanraja06@gmail.com>
2024-12-16 03:37:35 +0500

    commit

--- app.js
+++ app.js
@@ -15,25 +15,22 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(cookieParser());
 app.get("/", (req, res) => res.render("index"));
 
-
-app.post("/create", async (req, res) => {
-    
+app.post("/create", (req, res) => {
   let { username, password, email, age } = req.body;
 
-bcrypt.genSalt(10,(err,salt)=>{
-bcrypt.hash(password,salt , (err,hash)=>{
-    
-})    
-})
+  bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, async (err, hash) => {
+      console.log(hash);
+      let createdUser = await userModel.create({
+        username,
+        password,
+        email,
+        age,
+      });
 
-//   let createdUser = await userModel.create({
-//     username,
-//     password,
-//     email,
-//     age,
-//   });
-
-//   res.send(createdUser);
+      res.send(createdUser);
+    });
+  });
 });
 
 app.listen(3000);
