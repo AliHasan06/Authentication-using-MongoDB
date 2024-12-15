aaf47e3226989aba07d38a4f51b1d25d102fb5d8
AliHasan06 <alihassanraja06@gmail.com>
2024-12-16 02:30:23 +0500

    commit

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-const express = require('express');
-const ejs = require('ejs');
+const express = require("express");
+const ejs = require("ejs");
 const app = express();
-app.set('view engine', 'ejs');
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
-app.get('/',(req,res)=>
-    res.send('Hello, World!'))
+const path = require("path");
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.get("/", (req, res) => res.send("Hello, World!"));
 
-app.listen(3000)
+app.listen(3000);
