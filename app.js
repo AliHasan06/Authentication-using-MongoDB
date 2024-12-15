315d965cd85775108645a1b7cd7767202ea17e1e
AliHasan06 <alihassanraja06@gmail.com>
2024-12-16 03:33:34 +0500

    commit

--- app.js
+++ app.js
@@ -7,23 +7,33 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const userModel = require("./models/user");
 
-
 app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
- 
+
 app.use(cookieParser());
 app.get("/", (req, res) => res.render("index"));
-app.post("/create", async (req, res) => {
-    let {username, password,email,age} = req.body;
-let createdUser = await userModel.create({
-    username,
-    password,
-    email,
-    age
-});
-});
 
+
+app.post("/create", async (req, res) => {
+    
+  let { username, password, email, age } = req.body;
+
+bcrypt.genSalt(10,(err,salt)=>{
+bcrypt.hash(password,salt , (err,hash)=>{
+    
+})    
+})
+
+//   let createdUser = await userModel.create({
+//     username,
+//     password,
+//     email,
+//     age,
+//   });
+
+//   res.send(createdUser);
+});
 
 app.listen(3000);
